82272ca6686bfeeb9abcc7ae85616863a2cd66d7
Alwin Zhao <alwinzhao12@gmail.com>
2025-10-06 15:16:29 -0400

    create displays for home() and showAvailable()

--- src/main/java/com/pluralsight/NeighborhoodLibrary.java
+++ src/main/java/com/pluralsight/NeighborhoodLibrary.java
@@ -2,8 +2,22 @@ package com.pluralsight;
 
 import java.util.Scanner;
 
+
 public class NeighborhoodLibrary {
+
     public static Scanner myScanner = new Scanner(System.in);
+
+    public static int askUserInt(String question) {
+        System.out.println(question);
+        int response = myScanner.nextInt();
+        myScanner.nextLine(); // eat the line
+        return response;
+    }
+    public static String askUserStr(String question) {
+        System.out.println(question);
+        return myScanner.nextLine();
+    }
+
 
     public static void main(String[] args) {
         Book[] inventory = new Book[20];
@@ -12,25 +26,23 @@ public class NeighborhoodLibrary {
 
     // each screen will be displayed by invoking a different static method
     public static void home() {
-        System.out.println("""
+        int response = askUserInt("""
                 Store Home Screen:
                 
                 1 - Display Available Books
                 2 - Show Checked Out Books
-                3 - Exit Application
+                0 - Exit Application
                 """);
-
     }
 
-    public static int askUserInt(String question) {
-        System.out.println(question);
-        int response = myScanner.nextInt();
-        myScanner.nextLine(); // eat the line
-        return response;
-    }
 
     public static void showAvailable() {
-
+        int response = askUserInt("""
+                Available Books:
+                0 - Exit to Home
+                or
+                Select a book to check out:
+                """);
     }
 
     public static void showCheckedOut() {
