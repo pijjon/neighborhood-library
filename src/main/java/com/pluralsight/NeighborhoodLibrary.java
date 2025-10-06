a928a13f9c373ea5b9398843d9933b30ce78b9d2
Alwin Zhao <alwinzhao12@gmail.com>
2025-10-06 14:49:52 -0400

    Set up methods for each screen display

--- src/main/java/com/pluralsight/NeighborhoodLibrary.java
+++ src/main/java/com/pluralsight/NeighborhoodLibrary.java
@@ -1,7 +1,34 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class NeighborhoodLibrary {
+    public static Scanner myScanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         Book[] inventory = new Book[20];
+
     }
+
+    // each screen will be displayed by invoking a different static method
+    public static void home() {
+        System.out.println("""
+                Store Home Screen:
+                
+                1 - Display Available Books
+                2 - Show Checked Out Books
+                3 - Exit Application
+                """);
+
+    }
+
+    public static void showAvailable() {
+
+    }
+
+    public static void showCheckedOut() {
+
+    }
+
+
 }
