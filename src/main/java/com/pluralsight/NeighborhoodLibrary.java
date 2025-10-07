8535320607b9e79232bda2d98b87e654dd95048a
Alwin Zhao <alwinzhao12@gmail.com>
2025-10-07 01:09:22 -0400

    create display and checkOut functionality in showAvailable() method

--- src/main/java/com/pluralsight/NeighborhoodLibrary.java
+++ src/main/java/com/pluralsight/NeighborhoodLibrary.java
@@ -1,9 +1,93 @@
 package com.pluralsight;
 
+import java.beans.beancontext.BeanContextServiceAvailableEvent;
 import java.util.Scanner;
 
 
 public class NeighborhoodLibrary {
+
+    public static Book[] inventory = new Book[20];
+
+    public static void main(String[] args) {
+
+        home();
+    }
+
+    // each screen will be displayed by invoking a different static method
+    public static void home() {
+        boolean isRunning = true;
+
+        while (isRunning) {
+            String response = askUserStr("""
+                    Store Home Screen
+                    
+                    1 - Display Available Books
+                    2 - Show Checked Out Books
+                    X - Exit Application
+                    """);
+
+            switch (response.toLowerCase()) {
+                case "1":
+                    showAvailable();
+                    break;
+
+                case "2":
+                    showCheckedOut();
+                    break;
+
+                case "x":
+                    isRunning = false;
+                    break;
+
+                default:
+                    System.out.println("wthelly");
+            }
+
+        }
+    }
+
+    public static void showAvailable() {
+        boolean isRunning = true;
+        System.out.println("Available Books");
+        while (isRunning) {
+            for (Book book : inventory) {
+                System.out.printf("ID: %d Title: %s ISBN: %s", book.getId(), book.getTitle(), book.getIsbn());
+            }
+            String response = askUserStr("""
+                    X - Exit to Home
+                    or
+                    To check out a book, provide the ID:
+                    """);
+
+            if (response.equalsIgnoreCase("x")) {
+                isRunning = false;
+            }
+            else {
+                String name = askUserStr("What is your name?");
+                for (Book book : inventory) {
+                    if (book.getId() == Integer.getInteger(response)) {
+                        book.checkOut(name);
+                    }
+                }
+            }
+
+
+        }
+
+        // checkout the book, then prompt user if they wanna check out another book
+        // if yes, showAvailable();
+        // else return;
+    }
+
+    public static void showCheckedOut() {
+        String response = askUserStr("""
+                Checked Out Books
+                
+                X - Exit to Home
+                or
+                C - Check in one of these books:
+                """);
+    }
 
     public static Scanner myScanner = new Scanner(System.in);
 
@@ -13,40 +97,10 @@ public class NeighborhoodLibrary {
         myScanner.nextLine(); // eat the line
         return response;
     }
+
     public static String askUserStr(String question) {
         System.out.println(question);
         return myScanner.nextLine();
-    }
-
-
-    public static void main(String[] args) {
-        Book[] inventory = new Book[20];
-
-    }
-
-    // each screen will be displayed by invoking a different static method
-    public static void home() {
-        int response = askUserInt("""
-                Store Home Screen:
-                
-                1 - Display Available Books
-                2 - Show Checked Out Books
-                0 - Exit Application
-                """);
-    }
-
-
-    public static void showAvailable() {
-        int response = askUserInt("""
-                Available Books:
-                0 - Exit to Home
-                or
-                Select a book to check out:
-                """);
-    }
-
-    public static void showCheckedOut() {
-
     }
 
 
